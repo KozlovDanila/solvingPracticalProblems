bdfeb01b99d8425680c235275874e206732a4198
Danila Kozlov <d.kozlov@isimplelab.com>
2022-11-18 10:33:27 +0300

    Подсчет гласных и согласных. Написать программу, которая подсчитывает число гласных и согласных букв в заданной строке. Сделать это для английского языка, который имеет 5 гласных (a, e, i, o, u).

---
+++ src/_5VowelsAndConsonantsCounts.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class _5VowelsAndConsonantsCounts {
+	public static void main(String[] args) {
+
+		String str = "This is string value";
+
+		// Мое решение
+		Set<String> vowels = Set.of("a", "e", "i", "o", "u");
+		Map<Boolean, Long> collect = str.chars()
+				.filter(Character::isAlphabetic)
+				.boxed()
+				.collect(Collectors.partitioningBy(v -> vowels.contains(Character.toString((char) v.intValue())), Collectors.counting()));
+		System.out.println("Согласных " + collect.get(false));
+		System.out.println("Гласных " + collect.get(true));
+
+
+	}
+}
