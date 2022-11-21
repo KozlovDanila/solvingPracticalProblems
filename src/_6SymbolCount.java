f72bf0116262e458314620aaa044977a6e2fbf38
Danila Kozlov <d.kozlov@isimplelab.com>
2022-11-21 11:19:43 +0300

    Подсчет появлений некоторого символа. Написать программу, которая подсчитывает появление того или иного символа в строке.

---
+++ src/_6SymbolCount.java
@@ -0,0 +1,29 @@
+public class _6SymbolCount {
+
+	public static void main(String[] args) {
+		System.out.println(countSymbolInString("this is string", 's'));
+	}
+
+//    Решение через циклы
+//    static int countSymbolInString(String str, char symbol) {
+//        int count = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == symbol) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+
+	//    Решение через стримы
+//    static long countSymbolInString(String str, char symbol) {
+//        return str.chars().filter(v -> v == symbol)
+//                .count();
+//    }
+
+	// Интересное решение из книги
+	static long countSymbolInString(String str, char symbol) {
+		return str.length() - str.replace(String.valueOf(symbol), "").length();
+	}
+
+}
