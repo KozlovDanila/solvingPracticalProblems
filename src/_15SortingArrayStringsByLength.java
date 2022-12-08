8717ffc9b6e938b2093d98941e2e763954b45c3b
Danila Kozlov <d.kozlov@isimplelab.com>
2022-12-08 17:43:09 +0300

    Сортировка массива строк по длине. Написать программу, которая сортирует заданный массив по длине строк.

---
+++ src/_15SortingArrayStringsByLength.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class _15SortingArrayStringsByLength {
+
+    public static void main(String[] args) {
+        String[] strs = {
+                "fasdf1323423",
+                "123",
+                "213454",
+                "1"
+        };
+
+        sortingArrayStringsByLength(strs);
+
+        System.out.printf(Arrays.toString(strs));
+    }
+
+static String[] sortingArrayStringsByLength(String[] arr) {
+   return Arrays.stream(arr)
+           .sorted(Comparator.comparingInt(String::length))
+           .toArray(String[]::new);
+}
+}
