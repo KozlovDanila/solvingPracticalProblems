aa6764ae85584d285740e6fc94d65014a48d1587
Danila Kozlov <d.kozlov@isimplelab.com>
2022-12-21 11:20:42 +0300

    Проверка наличия подстроки в строке. Написать программу, которая проверяет, содержит ли заданная строку заданную подстроку.

---
+++ src/_16StringContainsSubString.java
@@ -0,0 +1,18 @@
+import java.util.regex.Pattern;
+
+public class _16StringContainsSubString {
+
+	public static void main(String[] args) {
+		System.out.println(contains("abc", "ca"));
+	}
+
+//	Моё решение
+//	static boolean contains(String main, String search) {
+//		return main.contains(search);
+//	}
+
+//	Решение из книги на основе ругялрных выражений
+	static boolean contains(String main, String search) {
+		return main.matches("(?i).*" + Pattern.quote(search) + ".*");
+	}
+}
