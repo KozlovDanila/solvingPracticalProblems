4ada433b025039947dcf094a4e0a874ee7528ede
Danila Kozlov <d.kozlov@isimplelab.com>
2022-12-27 17:19:07 +0300

    Подсчет числа появлений подстроки в строке. Написать программу, которая подсчитывает число появлений заданной строки в другой заданной строке.

---
+++ src/_17CountStringContainsSubString.java
@@ -0,0 +1,39 @@
+import java.util.regex.Pattern;
+
+public class _17CountStringContainsSubString {
+
+
+    public static void main(String[] args) {
+        System.out.println(countStringContainsSubString("строкастрокастрстрокастр", "стр"));
+    }
+
+    //    Моё решение
+//    static int countStringContainsSubString(String main, String search) {
+//        int result = 0;
+//        int i;
+//        while ((i = main.indexOf(search)) != -1) {
+//            result++;
+//            main = main.substring(i + search.length());
+//        }
+//        return result;
+//    }
+
+    //    Решение из книги 1
+//    static int countStringContainsSubString(String main, String search) {
+//        int position = 0;
+//        int count = 0;
+//        int n = search.length();
+//
+//        while ((position = main.indexOf(search, position)) != -1) {
+//            position = position + n;
+//            count++;
+//        }
+//        return count;
+//    }
+
+    //    Решение из книги 2
+static int countStringContainsSubString(String main, String search) {
+    int result = main.split(Pattern.quote(search), -1).length - 1;
+    return result < 0 ? 0 : result;
+}
+}
