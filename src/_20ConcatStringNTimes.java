f97a9828d2591ddd4bf1341a9e71f8b1420db0e7
Danila Kozlov <d.kozlov@isimplelab.com>
2023-01-10 15:27:41 +0300

    Конкатенирование одной и той же строки n раз. Написать программу, которая конкатенирует одну и ту же строку заданное число раз.

---
+++ src/_20ConcatStringNTimes.java
@@ -0,0 +1,32 @@
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class _20ConcatStringNTimes {
+
+    public static void main(String[] args) {
+        System.out.println(concatNTimes("a", 5));
+    }
+
+    // Моё решение
+//static String concatNTimes(String str, int count) {
+//    StringBuilder builder = new StringBuilder();
+//    for (int i = 0; i < count; i++) {
+//        builder.append(str);
+//    }
+//    return builder.toString();
+//    return str.repeat(count);
+//}
+
+//    // Решение из книги 3
+//static String concatNTimes(String str, int count) {
+//    return String.join(" ", Collections.nCopies(count, str));
+//}
+
+    // Решение из книги 4
+static String concatNTimes(String str, int count) {
+    return Stream.generate(()->str)
+            .limit(count)
+            .collect(Collectors.joining());
+}
+}
