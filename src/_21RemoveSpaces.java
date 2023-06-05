d3482dd37ee846eb6aa175c98f1e97c6ead8c511
Danila Kozlov <d.kozlov@isimplelab.com>
2023-06-05 16:56:11 +0300

    Удаление начальных и замыкающих пробелов

---
+++ src/_21RemoveSpaces.java
@@ -0,0 +1,11 @@
+public class _21RemoveSpaces {
+
+	public static void main(String[] args) {
+		String value = "\n \n \n hello \n \n \n";
+		String trim = value.trim();
+		String strip = value.strip();
+		String strip1 = value.stripIndent();
+		String strip2 = value.stripLeading();
+		String strip3 = value.stripTrailing();
+	}
+}
